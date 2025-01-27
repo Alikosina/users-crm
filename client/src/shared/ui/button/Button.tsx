9d2d997bfa7f896a4d31d60f594edd89defc4a38
Alik Sloyan <asloyan@vepay.online>
2025-01-27 16:19:03 +0300

    Added Storybook

--- client/src/shared/ui/button/Button.tsx
+++ client/src/shared/ui/button/Button.tsx
@@ -1,3 +1,5 @@
-export const Button = () => {
-  return <button></button>;
+type Props = { children: React.ReactNode };
+
+export const Button = ({ children }: Props) => {
+  return <button>{children}</button>;
 };
