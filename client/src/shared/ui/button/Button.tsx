e5b4d7a764ff0a37ff4d1c52e40957fd4bc3cdcf
Alik Sloyan <asloyan@vepay.online>
2025-01-27 21:57:02 +0300

    Added classnames package

--- client/src/shared/ui/button/Button.tsx
+++ client/src/shared/ui/button/Button.tsx
@@ -1,9 +1,14 @@
 import { ButtonHTMLAttributes } from "react";
+import cn from "classnames";
 
 import styles from "./Button.module.scss";
 
 type Props = ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const Button = ({ children }: Props) => {
-  return <button className={styles.button}>{children}</button>;
+export const Button = ({ children, className, onClick }: Props) => {
+  return (
+    <button onClick={onClick} className={cn(styles.button, className)}>
+      {children}
+    </button>
+  );
 };
--- client/src/shared/ui/input/Input.tsx
+++ client/src/shared/ui/input/Input.tsx
@@ -1,3 +1,16 @@
-export const Input = () => {
-  return <input />;
+import { InputHTMLAttributes } from "react";
+import cn from "classnames";
+
+import styles from "./Input.module.scss";
+
+type Props = InputHTMLAttributes<HTMLInputElement>;
+
+export const Input = ({ value, onChange, className }: Props) => {
+  return (
+    <input
+      value={value}
+      onChange={onChange}
+      className={cn(styles.input, className)}
+    />
+  );
 };
