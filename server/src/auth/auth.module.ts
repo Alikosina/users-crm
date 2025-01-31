793f88c7bd5cfda9e0aa7ef833fd7672fa83837b
Alik Sloyan <asloyan@vepay.online>
2025-01-31 19:16:29 +0300

    Removed unused variables

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -5,8 +5,6 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
 import { PrismaService } from '../prisma.service';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [
