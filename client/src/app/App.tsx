399cbea76b8a7468496448f1902696375c0b7a56
Alik Sloyan <asloyan@vepay.online>
2025-02-19 23:16:02 +0300

    Added Inject Store logic for axios interceptors

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,8 +1,11 @@
+import { injectStore } from "@/shared/lib";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router";
 
 import { appRouter } from "./router";
 import { store } from "./store";
+
+injectStore(store);
 
 export function App() {
   return (
--- client/src/shared/lib/apiClient.ts
+++ client/src/shared/lib/apiClient.ts
@@ -1,6 +1,22 @@
 import axios from "axios";
 import { apiUrl } from "../constants";
 
-export const apiClient = axios.create({
+let store: any;
+
+export const injectStore = (_store: any) => {
+  store = _store;
+};
+
+const apiClient = axios.create({
   baseURL: apiUrl,
 });
+
+// TODO: Add passing Access Token Logic and Refresh Token Logic
+apiClient.interceptors.request.use(async (req) => {
+  return req;
+});
+apiClient.interceptors.response.use(async (resp) => {
+  return resp;
+});
+
+export { apiClient };
