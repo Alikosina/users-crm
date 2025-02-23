c7da77d71b5f2706b2f79ca2074beb3d2d282009
Alik Sloyan <asloyan@vepay.online>
2025-02-23 07:40:16 +0300

    Fixed import

--- client/src/app/router.tsx
+++ client/src/app/router.tsx
@@ -1,7 +1,7 @@
 import { HomePage } from "../pages/home";
 import { LoginPage } from "../pages/login";
 import { createBrowserRouter } from "react-router";
-import { ProtectedRoute } from "@/entities/auth/ui";
+import { ProtectedRoute } from "@/entities/auth";
 
 export const appRouter = () =>
   createBrowserRouter([
